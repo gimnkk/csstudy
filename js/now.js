58ae59c63ac4e0507cf4089d2761c5cdca63a62d
DESKTOP-40KH4LC\gimnk <gimnkk@gmail.com>
2024-01-27 00:16:55 +0900

    edit clock

--- js/now.js
+++ js/now.js
@@ -6,13 +6,12 @@ function clock() {
     var month = date.getMonth();
     var clockDate = date.getDate();
     var day = date.getDay();
-
+    var weekStr = now.getWeek();
     var week = ['일', '월', '화', '수', '목', '금', '토'];
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
-    clockTarget.innerText = `${year}년 ${month + 1}월 ${clockDate}일 ${week[day]}요일 ` +
-
+    clockTarget.innerText = `${year}년 ${month + 1}월 ${clockDate}일 ${week[day]}요일 ${weekStr}주차` +
         `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
